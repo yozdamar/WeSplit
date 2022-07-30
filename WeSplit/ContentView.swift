ecd1f48b2e7081e0dce45cda0ebc0a7c77e7778c
yozdamar <yigit@ozdamar.net>
2022-07-30 12:08:29 +0400

    Reset everything from overview

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -13,36 +13,9 @@ struct ContentView: View {
     let students = ["Harry", "Ron", "Harmonie"]
     @State private var selectedStudent = "Harry"
     var body: some View {
-        NavigationView {
-            Form{
-                Section{
-                    Text("Text Number 1")
-                    Text("Text Number 2")
-                    Text("Text Number 3")
-                    Group{
-                        Text("Text Number 4")
-                    }
-                }
-                Section{
-                    Text("Here is another section")
-                }
-                .navigationTitle("SwiftUI")
-                .navigationBarTitleDisplayMode(.inline)
-                Button("Tap Count: \(tapCount)"){
-                    tapCount += 1
-                }
-                TextField("Enter Your Name", text: $name)
-                Text("Your name is \(name)")
-                Section{
-                    Picker("Select your student", selection: $selectedStudent){
-                        ForEach(students, id: \.self){
-                            Text($0)
-                        }
-                    }
-                }
-            }
-        }
-    }
+        Text("Hello, world!")
+            .padding()
+}
 }
 
 struct ContentView_Previews: PreviewProvider {
