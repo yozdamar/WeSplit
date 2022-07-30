d70ab8d19a0a7e662ee92f58c4cedf0c7efafab4
yozdamar <yigit@ozdamar.net>
2022-07-30 10:45:52 +0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Yigit on 30.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
